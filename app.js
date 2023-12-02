7e62708306e1db10186fec9cd00fedc59f7a3a5f
karodehemant9 <karode.hemant9@gmail.com>
2023-12-02 22:45:52 +0530

    Creating user collection

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 
 
@@ -21,14 +21,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   // User.findById('656af641aa51ed6bc064653c')
-//   //   .then(user => {
-//   //     req.user = new User(user.name, user.email, user.cart, user_id);
-//   //     next();
-//   //   })
-//   //   .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('656b62c3b0338f8bf3abb9d5')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +38,21 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://hemant:mongopassword@cluster0.f94zpeh.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+
+        const user = new User({
+          name: 'Hemant',
+          email: 'karode.hemant9@gmail.com',
+          cart: {
+            items: [],
+          }
+        });
+        user.save();
+
+      }
+    })
+
     app.listen(3000);
     console.log('server started!');
   })
